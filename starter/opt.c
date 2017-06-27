bfecde0eed352e899ab02204239647fb47e7d119
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-06-26 23:44:01 -0400

    need list

--- starter/opt.c
+++ starter/opt.c
@@ -4,6 +4,7 @@
 #include <getopt.h>
 #include <stdlib.h>
 #include "pagetable.h"
+#define MAXLINE 256
 
 
 extern int memsize;
@@ -34,6 +35,25 @@ void opt_ref(pgtbl_entry_t *p) {
  * replacement algorithm.
  */
 void opt_init() {
+	char buf[MAXLINE];
+	addr_t vaddr = 0;
+	char type;
+	FILE *tfp;
+	if(tracefile != NULL) {
+		if((tfp = fopen(tracefile, "r")) == NULL) {
+			perror("Error opening tracefile:");
+			exit(1);
+		}
+	}
+
+	while(fgets(buf, MAXLINE, tfp) != NULL) {
+		if(buf[0] != '=') {
+			sscanf(buf, "%c %lx", &type, &vaddr);
+		} else {
+			continue;
+		}
+	}
+	
 
 }
 
