c5d0602f4ba211ddca603b281b7ec54ab658985e
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-06-27 03:41:51 -0400

    test

--- starter/opt.c
+++ starter/opt.c
@@ -38,7 +38,12 @@ int opt_evict() {
 			}
 		}
 	}
-	printf("%d \n", evicted);
+	for (i = 0; i < memsize; i += 1) {
+		if (coremap[i].pte->checked != 1) {
+			evicted = i;
+			printf("%d \n", coremap[i].pte->checked)
+		}
+	}
 	
 	return evicted;
 }
