6cc0dd5576412414dc86b7b55bd0d27d5d7d4ed5
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-06-27 00:58:22 -0400

    opt

--- starter/opt.c
+++ starter/opt.c
@@ -31,7 +31,7 @@ int opt_evict() {
 	int longest = 0;
 	for (i = 0; i < memsize; i += 1) {
 		for (o = line; o <= filesize; o += 1) {
-			if ((coremap[i].pte->virtualaddress == addresslist[o]) && (o - line >= longest)){
+			if ((coremap[i].pte->virtualaddress == addresslist[o]) && ((o - line) >= longest)){
 				longest = o - line;
 				evicted = i;
 				coremap[i].pte->checked = 1;
