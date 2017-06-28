e61b191956017fa90a7f23eab39d039b8dad80f1
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-06-28 14:56:00 -0400

    opt

--- starter/opt.c
+++ starter/opt.c
@@ -29,20 +29,29 @@ int opt_evict() {
 	int evicted = 0;
 	int i,o;
 	int longest = 0;
+	//setup we check if all the next references are greater than the current, if not we need to update them
 	for (i = 0; i < memsize; i += 1) {
-		for (o = line + 1; o <= filesize; o += 1) {
-			if (coremap[i].pte->virtualaddress == addresslist[o] && o - line >= longest && coremap[i].pte->checked != 1){
-				longest = o - line;
-				evicted = i;
-				coremap[i].pte->checked = 1;
+		if (coremap[i].pte->nextreference < line){
+			coremap[i].pte->checked = 0;
+			for (o = line + 1; o <= filesize; o += 1) {
+				if (coremap[i].pte->virtualaddress == addresslist[o]){
+					coremap[i].pte->checked = 1;
+					coremap[i].pte->nextreference = o;
+				}
 			}
 		}
 	}
+	//we pick one that has the longest nextreference
+	for (i = 0; i < memsize; i += 1) {
+		if (coremap[i].nextreference > line && coremap[i].nextreference >= longest){
+			evicted = i;
+			longest = coremap[i].nextreference;
+		}
+	}
+	//we pick one that never has a check(so it never comes back)
 	for (i = 0; i < memsize; i += 1) {
 		if (coremap[i].pte->checked != 1) {
 			evicted = i;
-		} else {
-			coremap[i].pte->checked = 0;
 		}
 	}
 	
