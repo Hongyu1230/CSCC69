1516de40821c148ac050b523816918f1116ad5f0
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-07-19 00:10:25 -0400

    A3 setup

--- A3/readimage.c
+++ A3/readimage.c
@@ -61,6 +61,19 @@ int main(int argc, char **argv) {
 		printf(" ");
 	}
 	printf("\n");
-	
+	char* inodes = (char*)(disk + 1024 * bg->bg_inode_table);
+	struct ext2_inode *inode;
+	int* tbl = malloc(sizeof(int) * 32);
+	char type = "not set";
+	printf("Inodes:\n");
+	for (i = EXT2_ROOT_INO - 1; i < 32t; i+=1){
+		inode = (struct ext2_inode *) (inode_info + sizeof(struct ext2_inode) * i);
+		if (inode->i_mode & EXT2_S_IFREG) {
+			type = "f";
+		} else if (inode->i_mode & EXT2_S_IFDIR) {
+			type = "d";
+		}
+		printf("[%d] type: %c size: %d links: %d blocks: %d\n", i, type, inode->i_size, inode->i_links_count, inode->i_blocks);
+	}
     return 0;
 }
