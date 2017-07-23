51d27dde6146ac41f7b79b42f772ce9996f7e810
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-07-23 01:37:43 -0400

    cp

--- A3/ext2_cp.c
+++ A3/ext2_cp.c
@@ -38,8 +38,6 @@ int main(int argc, char **argv) {
 		i -= 1;
 	}
 	printf("%d", i);
-	strncpy(sourcename, &sourcepath[i], strlen(sourcepath) - i);
-	printf("%s", sourcename);
     disk = mmap(NULL, 128 * 1024, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if(disk == MAP_FAILED) {
 		perror("mmap");
