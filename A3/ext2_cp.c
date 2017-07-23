c099c978c3d5dd9eecec28f980245f77b68c0139
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-07-23 01:16:15 -0400

    cp

--- A3/ext2_cp.c
+++ A3/ext2_cp.c
@@ -29,9 +29,7 @@ int main(int argc, char **argv) {
 	char destpath[strlen(argv[3])];
 	strcpy(sourcepath, argv[1]);
 	strcpy(destpath, argv[1]);
-	if (destpath[0] != '/') {
-		return ENOENT;
-	}
+
 	printf("%s", sourcepath);
 	int i;
 	while (destpath[i] != '/' || i != 0) {
