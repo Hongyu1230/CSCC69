6a8ac682636b6e896a2d25779a8353f3a474b086
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-07-24 00:35:20 -0400

    derp

--- A3/ext2_cp.c
+++ A3/ext2_cp.c
@@ -58,7 +58,7 @@ int main(int argc, char **argv) {
 	int sizecheck = 0;
 	struct ext2_dir_entry_2 *directory;
 	while (token2 != NULL && pathnode->i_mode & EXT2_S_IFDIR && blocktrack <= 11) {
-		directory = (struct ext2_dir_entry_2 *)(disk + 1024 * pathnode[blocktrack]);
+		directory = (struct ext2_dir_entry_2 *)(disk + 1024 * pathnode->i_block[blocktrack]);
 		while (sizecheck < pathnode->i_size) {
 			if(strcmp(token2, directory->name)) {
 				pathnode = itable + directory->inode - 1;
