b8091c8504fd9dfb289b20acf574beea03d1a39a
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-07-24 00:32:49 -0400

    derp

--- A3/ext2_cp.c
+++ A3/ext2_cp.c
@@ -54,7 +54,7 @@ int main(int argc, char **argv) {
 	struct ext2_inode *itable = (struct ext2_inode *)(disk + 1024 * bg->bg_inode_table);
 	struct ext2_inode *pathnode = itable + 1;
 	token2 = strtok(destpath, delimiter);
-	int blcoktrack = 0;
+	int blocktrack = 0;
 	int sizecheck = 0;
 	struct ext2_dir_entry_2 *directory;
 	while (token2 != NULL && pathnode->i_mode & EXT2_S_IFDIR && blocktrack <= 11) {
