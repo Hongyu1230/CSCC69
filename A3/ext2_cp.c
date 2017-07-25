209ab88a1f385396ac94d8f3b66625208ff35d26
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-07-24 23:04:44 -0400

    test

--- A3/ext2_cp.c
+++ A3/ext2_cp.c
@@ -80,6 +80,7 @@ int main(int argc, char **argv) {
                     check = 1;
 					found = 1;
                     token2 = strtok(NULL, delimiter);
+					printf("%s\n", token2);
                     break;
                 } else {
                     sizecheck += directory->rec_len;
@@ -93,7 +94,7 @@ int main(int argc, char **argv) {
         }
 		if (found == 1) {
 			found = 0;
-			printf("reached");
+			printf("reached\n");
 		} else {
 			perror("cannot find destination directory on disk");
 			return ENOENT;
