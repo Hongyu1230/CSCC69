ad67befea9dda7bfbf6de0f8edf18a5ff45f39a4
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-07-25 19:58:31 -0400

    new stuff

--- A3/ext2_cp.c
+++ A3/ext2_cp.c
@@ -209,10 +209,9 @@ int main(int argc, char **argv) {
         }
     }
     newnode->i_mode = EXT2_S_IFREG;
-    newnode->i_size = 25;
+    newnode->i_size = filesize;
     newnode->i_blocks = blockneeded * 2;
     newnode->i_links_count = 1;
-	newnode->i_dtime = 0;
     struct ext2_dir_entry_2 *oldentry;
     struct ext2_dir_entry_2 *newentry;
     int spaceneeded = 8 + lengthcomp + (4 - lengthcomp % 4);
