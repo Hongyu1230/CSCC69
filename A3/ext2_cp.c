b35345b07cc9e2652a264928860a537966c873de
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-07-25 16:00:38 -0400

    new stuff

--- A3/ext2_cp.c
+++ A3/ext2_cp.c
@@ -223,11 +223,11 @@ int main(int argc, char **argv) {
 		sizecheck = 0;
 		while (sizecheck < 1024) {
 			sizecheck += oldentry->rec_len;
-			if (sizecheck = 1024 && oldentry->rec_len >= 4*oldentry->name_len + lengthcomp + 8){
+			if (oldentry->rec_len >= (2 * sizeof(struct ext2_dir_entry_2)) + lengthcomp + (4 - lengthcomp % 4)) + oldentry->name_len + (4 - (oldentry->name_len % 4)){
 				check = 1;
 				paddingneeded2 = 4 - oldentry->name_len % 4;
 				oldsize = oldentry->rec_len;
-				oldentry->rec_len = 4*oldentry->name_len + 8;
+				oldentry->rec_len = sizeof(struct ext2_dir_entry_2) + oldentry->name_len + (oldentry->name_len % 4);
 				newentry = oldentry = (void *) oldentry + oldentry->rec_len;
 				newentry->inode = free_inode;
 				newentry->rec_len = oldsize - oldentry->rec_len;
