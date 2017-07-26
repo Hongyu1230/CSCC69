1cce6f94700e7c6abaff678c4fd2a008800444bc
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-07-26 04:02:01 -0400

    fix

--- A3/ext2_mkdir.c
+++ A3/ext2_mkdir.c
@@ -66,7 +66,7 @@ int main(int argc, char **argv) {
     int sizecheck, check, blockpointer, found, lengthcomp, startingpoint, immediatebreak = 0;
 	int storedlocation = 1;
     struct ext2_dir_entry_2 *directory;
-    while (token2 != NULL && S_ISDIR(pathnode->i_mode)) {
+    while (token2 != NULL && S_ISDIR(pathnode->i_mode) && startingpoint < pathlocation - 1) {
         lengthcomp = strlen(token2);
         for (blockpointer = 0; blockpointer < 12; blockpointer+=1) {
             directory = (struct ext2_dir_entry_2 *)(disk + 1024 * pathnode->i_block[blockpointer]);
