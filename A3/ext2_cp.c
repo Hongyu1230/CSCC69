bb3af9283f65c8737185e44f3ee44e01594072dc
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-07-25 23:38:42 -0400

    derp

--- A3/ext2_cp.c
+++ A3/ext2_cp.c
@@ -263,7 +263,7 @@ int main(int argc, char **argv) {
     bbmap = (char *)(disk + 1024 * bg->bg_block_bitmap);
     for (i = 0; i < 16; i+=1, bbmap +=1) {
         for (pos = 0; pos < 8; pos+=1) {
-			*bbmap >> pos | = block_bitmap[(8 * i) + pos]
+			*bbmap >> pos | = block_bitmap[(8 * i) + pos];
         }
     }	
 	
