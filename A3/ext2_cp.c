d5912f4cd364a59877809d5d874c10f89b1762a5
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-07-25 23:38:06 -0400

    derp

--- A3/ext2_cp.c
+++ A3/ext2_cp.c
@@ -262,12 +262,18 @@ int main(int argc, char **argv) {
     
     bbmap = (char *)(disk + 1024 * bg->bg_block_bitmap);
     for (i = 0; i < 16; i+=1, bbmap +=1) {
+        for (pos = 0; pos < 8; pos+=1) {
+			*bbmap >> pos | = block_bitmap[(8 * i) + pos]
+        }
+    }	
+	
+	bbmap = (char *)(disk + 1024 * bg->bg_block_bitmap);
+	for (i = 0; i < 16; i+=1, bbmap +=1) {
         temp = *bbmap;
         for (pos = 0; pos < 8; pos+=1) {
             block_bitmap[(8 * i) + pos] = (temp >> pos) & 1;
         }
     }
-	
 	printf("checkpoint");
 	for (i = 0; i < 16; i+=1, bbmap +=1) {
         for (pos = 0; pos < 8; pos+=1) {
