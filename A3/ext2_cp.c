460bbfec96874610d981bd16fb590d2039bc65ee
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-07-26 19:05:21 -0400

    wtf

--- A3/ext2_cp.c
+++ A3/ext2_cp.c
@@ -167,7 +167,7 @@ int main(int argc, char **argv) {
             if (block_bitmap[j] == 0) {
                 block_bitmap[j] = 1;
                 newnode->i_block[i] = j + 1;
-                memcpy(disk + 1024 * (j + 1), src + 1024*i, 1024;
+                memcpy(disk + 1024 * (j + 1), src + 1024*i, 1024);
                 break;
             }
         }
