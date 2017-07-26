76a9e71cfae85ec0fc0b8047d0dcdddff77797a9
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-07-26 00:16:23 -0400

    derp

--- A3/ext2_cp.c
+++ A3/ext2_cp.c
@@ -261,6 +261,14 @@ int main(int argc, char **argv) {
         strncpy(newentry->name, sourcename, lengthcomp);
     }
     printf("\n");
+	printf("checkpoint");
+    for (i = 0; i < 16; i+=1, bbmap +=1) {
+        for (pos = 0; pos < 8; pos+=1) {
+            printf("%d", block_bitmap[(8 * i) + pos]);
+        }
+        printf(" ");
+    }
+    printf("\n");
     bbmap = (char *)(disk + 1024 * bg->bg_block_bitmap);
     for (i = 0; i < 16; i+=1, bbmap +=1) {
         for (pos = 0; pos < 8; pos+=1) {
@@ -277,7 +285,7 @@ int main(int argc, char **argv) {
             block_bitmap[(8 * i) + pos] = (temp >> pos) & 1;
         }
     }
-    printf("checkpoint");
+    printf("checkpoint2");
     for (i = 0; i < 16; i+=1, bbmap +=1) {
         for (pos = 0; pos < 8; pos+=1) {
             printf("%d", block_bitmap[(8 * i) + pos]);
