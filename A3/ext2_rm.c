dcbcaea8caaad4c57af91d090b46449b409c1ee5
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-07-28 01:26:42 -0400

    hey

--- A3/ext2_rm.c
+++ A3/ext2_rm.c
@@ -184,6 +184,7 @@ int main(int argc, char **argv) {
                 if (sizecheck > 0) {
 					oldentry = (void *) oldentry - oldlen;
                     oldentry->rec_len += deletiondirectory->rec_len;
+					memset(deletiondirectory, 0, deletiondirectory->rec_len);
                     break;
                 } else {
                     oldsize = deletiondirectory->rec_len;
