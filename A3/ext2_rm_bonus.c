34d958a02cbaad7a6ea407480a98f488875e22b9
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-07-30 15:26:31 -0400

    new stuff

--- A3/ext2_rm_bonus.c
+++ A3/ext2_rm_bonus.c
@@ -221,7 +221,7 @@ int main(int argc, char **argv) {
 			} else {
 		        sprintf(command, "./ext2_rm %s %s/%s", argv[1 + r], argv[2 + r], oldname);
 			}
-		    printf("%s", command);
+		    printf("%s\n", command);
         }
         if (check == 1){
             break;
