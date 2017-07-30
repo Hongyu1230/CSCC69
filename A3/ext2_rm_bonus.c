cf97d1646c88174d0702633b9388e4264c299e39
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-07-30 12:36:32 -0400

    test

---
+++ A3/ext2_rm_bonus.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <string.h>
+#include <math.h>
+#include "ext2.h"
+
+unsigned char *disk;
+
+
+int main(int argc, char **argv) {
+
+    system("./ext2_mkdir emptydisk.img /tester123")
+    return 0;
+}
