0783430647f73d11d709042f401a2f6b03b0fda1
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-07-30 00:58:46 -0400

    good shit

--- A3/ext2_mkdir.c
+++ A3/ext2_mkdir.c
@@ -27,7 +27,6 @@ int main(int argc, char **argv) {
     strcpy(destpath2, argv[2]);
     if (destpath[0] != '/') {
 		printf("%s", destpath);
-        printf("the path needs to start from root, beginning with /\n");
         return ENOENT;
     }
     if (strlen(destpath) == 1) {
