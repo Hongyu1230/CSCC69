a60b7bbbdb6a6376b6ec78080b7f4b1324483d6f
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-07-30 23:02:06 -0400

    yolo

--- A3/ext2_cp.c
+++ A3/ext2_cp.c
@@ -205,10 +205,10 @@ int main(int argc, char **argv) {
                 }
             }
         }
-		printf("%d\n", free_inode);
         for (n = 0; n < blockneeded - 12; n+=1) {
             memcpy(disk + 1024 * indirectionblock[n], src + 1024*(n+12), 1024);
         }
+		printf("%d\n", free_inode);
     }
     newnode->i_mode = EXT2_S_IFREG | S_IRWXO;
     newnode->i_size = filesize;
