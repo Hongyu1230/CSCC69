a10f13e6e72f768fdf3f2ce3eba9a606df6e8a3d
Hongyu1230 <hongyu.lin@mail.utoronto.ca>
2017-07-31 11:27:01 -0400

    test

--- A3/ext2_rm.c
+++ A3/ext2_rm.c
@@ -177,7 +177,9 @@ int main(int argc, char **argv) {
 		int *indirectionblock = (void *) (disk + 1024 * deletionnode->i_block[12]);
 		int indirectionfreed = 0;
 		while (indirectionblock[indirectionfreed] != 0) {
-			printf("%d\n", indirectionblock[indirectionfreed]);
+			if (indirectionblock[indirectionfreed] == NULL){
+				printf("%d\n", indirectionblock[indirectionfreed]);
+			}
 			if (indirectionblock[indirectionfreed] > 128){
 				break;
 			}
